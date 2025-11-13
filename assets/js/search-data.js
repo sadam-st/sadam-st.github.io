05ae7ee5087618db84b348289879979e7c3f348e
sadam-st <92644060+sadam-st@users.noreply.github.com>
2025-11-13 00:19:49 +0000

    Deploying to gh-pages from @ sadam-st/sadam-st.github.io@a747807342cb53b01e1795928b220bd2673ba8d0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -448,8 +448,8 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-three-papers-accepted-at-spie-medical-imaging-conference-2026-vancouver-canada-sparkles-smile",
+          title: 'Three papers accepted at SPIE Medical Imaging Conference 2026 Vancouver, Canada.! :sparkles: :smile:...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
